c752cc71a2ddd9d829802b0a2525de7c1d65310d
Aishaarain <155973754+Aishaarain@users.noreply.github.com>
2025-07-23 17:08:19 +0500

    added pages and sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import Dashboard from '../src/pages/Dashboard'
+import { useLocation } from 'react-router-dom';
+import { Router, Routes, Route } from 'react-router-dom';
+import Sidebar from './components/Mainsidebar';
+import FoodPage from './pages/Foodmenu';
+import Navbar from './components/Navbar';
+import {Menu} from 'lucide-react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+const location = useLocation();
+  const hiddenNavRoutes = ['/'];
+  const isNavbarHidden = hiddenNavRoutes.includes(location.pathname);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+    {/* <Navbar/> */}
+   
+      <div className="flex">
+        
+ <div
+        className={`${
+          sidebarOpen ? 'block' : 'hidden'
+        } fixed top-0 right-0 w-[250px] h-screen bg-white shadow-lg z-40  transition-transform duration-300 lg:hidden`}
+      >
+        <Sidebar setSidebarOpen={setSidebarOpen} sidebarOpen={sidebarOpen} />
       </div>
-      <h1 className='text-pink-500'>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+<div className='max-md:hidden min-h-screen bg-[#5D3FD3]'>
+      <Sidebar />
+ </div>
+        <main className="flex-1 bg-[#F3F4FF] min-h-screen">
+          {!isNavbarHidden && <Navbar setSidebarOpen={setSidebarOpen} />}
+
+          <Routes>
+            <Route path="/" element={<Dashboard setSidebarOpen={setSidebarOpen} />} />
+            {/* <Route path="/students" element={<Students />} />
+            <Route path="/teachers" element={<Teachers />} />
+            <Route path="/event" element={<Event />} />
+            <Route path="/finance" element={<Finance />} /> */}
+            <Route path="/food" element={<FoodPage />} />
+            {/* <Route path="/user" element={<User />} />
+            <Route path="/chat" element={<Chat />} />
+            <Route path="/activity" element={<Activity />} /> */}
+          </Routes>
+        </main>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    
+    </div>
   )
 }
 
 export default App
+
